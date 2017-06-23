c7553c2a6c26b5b1c1f6aae52106c1ac40a8b6d1
ryanhanhui <wyhanhui@163.com>
2017-06-23 22:01:17 +0800

    文件上传调整

--- PRBook2.0/Controllers/FileUploadController.cs
+++ PRBook2.0/Controllers/FileUploadController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace PRBook2._0.Controllers
 {
@@ -16,29 +17,65 @@ namespace PRBook2._0.Controllers
         {
             return View();
         }
-        public string UploadFile()
+        /// <summary>
+        /// 单文件上传
+        /// </summary>
+        /// <returns></returns>
+        public string UploadSingleFile()
         {
+            string filetype = string.Empty, folder = string.Empty, oldpath = string.Empty;
+            string newfilepath = string.Empty, newfile = string.Empty;
+            if (Request["type"] != null)
+                filetype = Request["type"].ToString();
+            if (Request["folder"] != null)
+                folder = Request["folder"].ToString();
+            if (Request["oldpath"] != null)
+                oldpath = Request["oldpath"].ToString();
+
+            if (filetype.Equals("1"))
+            {
+                oldpath = Request.ApplicationPath + sysDown + oldpath;
+                newfilepath = Request.ApplicationPath + sysDown + folder + "/";
+            }
+            else if (filetype.Equals("0"))
+            {
+                oldpath = Request.ApplicationPath + userDown + oldpath;
+                newfilepath = Request.ApplicationPath + userDown + folder + "/";
+            }
+            try
+            {
+                if(!string.IsNullOrWhiteSpace(Request["oldpath"]))
+                    System.IO.File.Delete(Server.MapPath(oldpath));
+            }
+            catch
+            { }
+            if (!Directory.Exists(Server.MapPath(newfilepath)))
+            {
+                Directory.CreateDirectory(Server.MapPath(newfilepath));
+            }
             if (Request.Files.Count == 0)
             {
                 //Request.Files.Count 文件数为0上传不成功
-                return "上传失败";
+                return "Error";
             }
 
             var file = Request.Files[0];
             if (file.ContentLength == 0)
             {
                 //文件大小大（以字节为单位）为0时，做一些操作
-                return "文件大小大（以字节为单位）为0时，做一些操作";
+                return "Error0";
             }
             else
             {
                 //文件大小不为0
                 file = Request.Files[0];
                 //保存成自己的文件全路径,newfile就是你上传后保存的文件,
-                //服务器上的UpLoadFile文件夹必须有读写权限　　　　　　
-                file.SaveAs(Server.MapPath(@""+sysDown + "SysConfig/"+file.FileName));
-                return "上传成功";
+                //服务器上的UpLoadFile文件夹必须有读写权限
+                newfile = folder + "/" + file.FileName;
+                string newfilename = newfilepath + file.FileName;
+                file.SaveAs(Server.MapPath(newfilename));
+                return newfile;
             }
         }
-	}
+    }
 }
