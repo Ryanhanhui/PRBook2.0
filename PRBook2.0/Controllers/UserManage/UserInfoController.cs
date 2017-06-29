01159e65631e194238e197e344520b2cbf2bee85
ryanhanhui <wyhanhui@163.com>
2017-06-29 22:25:09 +0800

    个人信息修改

---
+++ PRBook2.0/Controllers/UserManage/UserInfoController.cs
@@ -0,0 +1,51 @@
+﻿using PRBook2._0.Models;
+using PRBook2._0.Models.Tool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PRBook2._0.Controllers.UserManage
+{
+    public class UserInfoController : Controller
+    {
+        PublicUtil util = new PublicUtil();
+        PRBook2._0.Models.LogicL.UserManage.UserInfo usi = new Models.LogicL.UserManage.UserInfo();
+        //
+        // GET: /UserInfo/
+        [Authorize]
+        public ActionResult Index()
+        {
+            if (!util.CheckPower(Request.RawUrl.ToString()))
+                return PartialView("WithoutPower");
+            return PartialView();
+        }
+        [Authorize]
+        public ActionResult EditPage()
+        {
+            if(Request["UserId"]!=null)
+            {
+                ViewBag.RStatus = "edit";
+                PR_UserInfo pr = usi.GetEditInfo(Request["UserId"].ToString());
+                ViewBag.Model = pr;
+            }
+            return View();
+        }
+        public string UpdateData()
+        {
+            PR_UserInfo pr = new PR_UserInfo();
+            pr.Id = int.Parse(Request.Params["Id"].ToString());
+            pr.UserId = Request.Params["UserId"].ToString();
+            pr.NickName = Request.Params["NickName"].ToString();
+            pr.Name = Request.Params["Name"].ToString();
+            pr.Sex = Request.Params["Sex"].ToString();
+            if (string.IsNullOrWhiteSpace(Request.Params["Age"]))
+                pr.Age = null;
+            else
+                pr.Age = int.Parse(Request.Params["Age"]);
+            pr.Remark = Request.Params["Remark"].ToString();
+            return usi.UpdateData(pr);
+        }
+	}
+}
---
+++ PRBook2.0/Models/LogicL/UserManage/UserInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+
+namespace PRBook2._0.Models.LogicL.UserManage
+{
+    public class UserInfo
+    {
+        PRBookEntities mdb = new PRBookEntities();
+        public PR_UserInfo GetEditInfo(string userId)
+        {
+            PR_UserInfo userinfo = mdb.PR_UserInfo.Where(u => u.UserId.Equals(userId)).ToList().FirstOrDefault();
+            return userinfo;
+        }
+        public string UpdateData(PR_UserInfo userInfo)
+        {
+            DbEntityEntry<PR_UserInfo> entry = mdb.Entry<PR_UserInfo>(userInfo);
+            entry.State = System.Data.Entity.EntityState.Unchanged;
+            entry.Property("UserId").IsModified = true;
+            entry.Property("Name").IsModified = true;
+            entry.Property("NickName").IsModified = true;
+            entry.Property("Sex").IsModified = true;
+            entry.Property("Age").IsModified = true;
+            entry.Property("Remark").IsModified = true;
+            int ret = mdb.SaveChanges();
+            if (ret != 0)
+                return "success";
+            else
+                return "";
+        }
+        private bool IsExists(PR_UserInfo userInfo)
+        {
+            int ncount = mdb.PR_UserInfo.Where(u => u.UserId.Equals(userInfo.UserId)).ToList().Count;
+
+            if (ncount > 0)
+                return false;
+            else
+                return true;
+        }
+    }
+}
