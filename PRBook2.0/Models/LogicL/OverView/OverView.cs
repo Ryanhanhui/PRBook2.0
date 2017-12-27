1a52078e3628ae8f395c515e3ade6020be38fc38
ryanhanhui <wyhanhui@163.com>
2017-12-27 22:45:16 +0800

    --

--- PRBook2.0/Models/LogicL/OverView/OverView.cs
+++ PRBook2.0/Models/LogicL/OverView/OverView.cs
@@ -18,10 +18,18 @@ namespace PRBook2._0.Models.LogicL.OverView
                 DbQuery<v_moneygetgivestat> query = mdb.v_moneygetgivestat as DbQuery<v_moneygetgivestat>;
                 if (!string.IsNullOrWhiteSpace(queryobj.UserId))
                     query = query.Where(u => u.UserId.Equals(queryobj.UserId)) as DbQuery<v_moneygetgivestat>;
-                //if (!string.IsNullOrWhiteSpace(queryobj.MoneyDate))
-                //    query = query.Where(u => u.NickName.Contains(userinfo.NickName)) as DbQuery<v_userinfo>;
-
-                query = query.OrderBy(u => u.Id) as DbQuery<v_moneygetgivestat>;
+                if (!string.IsNullOrWhiteSpace(queryobj.MoneyDate))
+                {
+                    string datestr = "1900-1";
+                    if(!queryobj.MoneyDate.Equals("-1"))
+                    {
+                        DateTime tmpdt = DateTime.Now.Date.AddMonths(-int.Parse(queryobj.MoneyDate));
+                        datestr = tmpdt.Year + "-" + (tmpdt.Month < 10 ? ("0" + tmpdt.Month) : (""+tmpdt.Month));
+                        query = query.Where(u => u.MoneyDate.CompareTo(datestr) >= 0) as DbQuery<v_moneygetgivestat>;
+                    }
+                    
+                }
+                query = query.OrderBy(u => u.MoneyDate) as DbQuery<v_moneygetgivestat>;
 
                 List<v_moneygetgivestat> pruserinfo = query.ToList();
                 return putil.GetJsonData(pruserinfo);
